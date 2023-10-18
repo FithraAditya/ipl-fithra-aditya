1314496137e926df7c35a8515d653a1be75e6ba6
FithraAditya <fithraaditya@gmail.com>
2023-10-18 07:44:41 +0700

    Menambahkan Order History, Viewer, Class Main

--- srp/Order.java
+++ srp/Order.java
@@ -1,25 +1,47 @@
 class Order {
-void calculateTotalSum() {
-/*...*/
+    void calculateTotalSum() {
+    /*...*/
+    }
+    void getItems() {
+    /*...*/
+    }
+    void getItemCount() {
+    /*...*/
+    }
+    void addItem(Item item) {
+    /*...*/
+    }
+    void deleteItem(Item item) {
+    /*...*/
+    }
+    void printOrder() {
+    /*...*/
+    }
+    void showOrder() {
+    /*...*/
+    }
 }
-void getItems() {
-/*...*/
+
+class OrderHistory {
+    void getDailyHistory() {}
+    void getMonthlyHistory() {}
 }
-void getItemCount() {
-/*...*/
+
+class OrderViewer {
+    void printOrder(Order order) {
+    }
+    void showOrder(Order order) {
+    }
 }
-void addItem(Item item) {
-/*...*/
-}
-void deleteItem(Item item) {
-/*...*/
-}
-void printOrder() {
-/*...*/
-}
-void showOrder() {
-/*...*/
-}
-void getDailyHistory() {}
-void getMonthlyHistory() {}
+
+class Main {
+    public static void main(String[] args) {
+        Item item = new Item();
+        Order order = new Order();
+        order.addItem(item);
+        OrderHistory history = new OrderHistory();
+        history.getDailyHistory();
+        OrderViewer viewer = new OrderViewer();
+        viewer.printOrder(order);
+    }
 }
